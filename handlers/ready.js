6ae230f2ff1e6164b90ed4c2683434017ec22335
MacTavishH6 <64173928+MacTavishH6@users.noreply.github.com>
2020-10-07 09:55:40 +0700

    update ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -4,5 +4,5 @@ const config = require("../config.json");
 AMbot.login(config.token);
 AMbot.on('ready', async() => {
     console.log(`${AMbot.user.username} is Online!`);
-    AMbot.user.setActivity(`Invite_me ${config.activity.game} | !help`);
+    AMbot.user.setActivity(`Invite_me ${config.activity.game} | !help`, { type: "CUSTOM_STATUS" });
 })
