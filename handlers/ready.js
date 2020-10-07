6f1e43dbdb51e7b1fa86e1b8fdd02b0576197876
MacTavishH6 <64173928+MacTavishH6@users.noreply.github.com>
2020-10-07 09:47:12 +0700

    change activity

--- handlers/ready.js
+++ handlers/ready.js
@@ -4,5 +4,5 @@ const config = require("../config.json");
 AMbot.login(config.token);
 AMbot.on('ready', async() => {
     console.log(`${AMbot.user.username} is Online!`);
-    AMbot.user.setActivity(`${config.activity.game} | !help`, { type: "PLAYING" });
+    AMbot.user.setActivity(`${config.activity.game} | !help`, { type: "INVITE_ME" });
 })
