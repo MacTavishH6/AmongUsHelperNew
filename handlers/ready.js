610ca82aa15678b6c53023396f852bee82d7cda2
MacTavishH6 <64173928+MacTavishH6@users.noreply.github.com>
2020-10-07 09:52:55 +0700

    change ready.js

--- handlers/ready.js
+++ handlers/ready.js
@@ -4,5 +4,5 @@ const config = require("../config.json");
 AMbot.login(config.token);
 AMbot.on('ready', async() => {
     console.log(`${AMbot.user.username} is Online!`);
-    AMbot.user.setActivity(`${config.activity.game} | !help`, { type: "INVITE_ME" });
+    AMbot.user.setActivity(`Invite_me ${config.activity.game} | !help`);
 })
